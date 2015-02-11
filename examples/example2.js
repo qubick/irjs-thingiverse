d0cfb93eb15805055db4766571eff32c71c94075
qubick <qubick.kim@gmail.com>
2015-02-11 14:00:16 -0700

    lastest changes

--- examples/example2.js
+++ examples/example2.js
@@ -2,5 +2,6 @@ var thing = require('../lib')
 
 //thing.searchByTerm('robot');
 //thing.searchByTag('figurine')
-//thing.getThingById('996355', 'ex'),
-thing.getThingsFromTo(274500,274541);
+thing.getThingById('996355', 'ex')//,
+//thing.getThingsFromTo(274500,274541)//,
+//thing.createThing(1,2,3,4,5,6,7);
