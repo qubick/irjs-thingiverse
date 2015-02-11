2d7761e2fe4f85b7c8facb80e26d562470eefce3
vicki388 <vickieverts1@gmail.com>
2015-02-11 11:27:25 -0700

    added file-exists to test

--- test/search.test.js
+++ test/search.test.js
@@ -1,50 +1,53 @@
 var streamify = require('stream-array'),
     assert = require('stream-assert'),
     should = require('should')()
+//var refine = require('../lib/v2')
+var thing = require('../lib')
+//var transform = refine.transform
+//var select = refine.select
+var fileExists = require('file-exists')
 
-var refine = require('../lib/v2')
-var transform = refine.transform
-var select = refine.select
+describe('thing', function() {
+// describe('search', function() {
+//     this.timeout(5000);
 
-describe('search', function() {
+//     it('should change the text in a cell to a link from Google', function(done) {
+
+//         streamify([
+//             ['google', 'b', 'c']
+//         ])
+//             .pipe(refine.start())
+//             .pipe(select.cols(0, transform.search()))
+//             .pipe(refine.end())
+
+//             .pipe(assert.first(function(row) {
+//                 row.should.be.eql(['http://www.google.com/', 'b', 'c'])
+//             }))
+//             .pipe(assert.end(done))
+
+//     })
+
+
+// })
+
+describe('searchByUser', function() {
     this.timeout(5000);
 
-    it('should change the text in a cell to a link from Google', function(done) {
+    it('should return true if the thingiverse work by a particular user has been saved to file', function(done) {
 
         streamify([
-            ['google', 'b', 'c']
+            ['mowi', 'zefram']
         ])
-            .pipe(refine.start())
-            .pipe(select.cols(0, transform.search()))
-            .pipe(refine.end())
+                .pipe(thing.searchByUser())
+                .pipe(assert(function() {
+                    //data = file-exists('file/Iphone 4 holder for Honda Jazz.zip')
+                    file = console.log(fileExists('../examples/html_json.txt'))
+                    file.should.be.eql(true)
+                }))
+                //.pipe(assert.length(1))
+                .pipe(assert.end(done))
 
-            .pipe(assert.first(function(row) {
-                row.should.be.eql(['http://www.google.com/', 'b', 'c'])
-            }))
-            .pipe(assert.end(done))
-
-    })
-
-
-})
-
-describe('search', function() {
-    this.timeout(5000);
-
-    it('should return the thingiverse work by a particular user', function(done) {
-
-        streamify([
-            ['zefram', 'eried', 'shira']
-        ])
-            .pipe(refine.start())
-            .pipe(select.cols(0, transform.search()))
-            .pipe(refine.end())
-
-            .pipe(assert.first(function(row) {
-                row.should.be.eql(['http://www.google.com/', 'b', 'c'])
-            }))
-            .pipe(assert.end(done))
-
+        })
     })
 })
-
+ //console.log(fileExists('../examples/html_json.txt'))
