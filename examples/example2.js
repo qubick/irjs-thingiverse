e10d6a92dbaf78abd5dda3b3e7ea96c4b81e083f
qubick <qubick.kim@gmail.com>
2015-02-14 14:33:46 -0700

    test case function names

--- examples/example2.js
+++ examples/example2.js
@@ -1,7 +1,7 @@
 var thing = require('../lib')
 
 //thing.searchByWord('robot');
-thing.searchByTag('figurine')
-//thing.getThingById('996355', 'ex')//,
-//thing.getThingsFromTo(274500,274541)//,
+//thing.searchByTag('figurine')
+//thing.getFileById('996355', 'ex')//,
+thing.getFilesFromTo(274500,274541)//,
 //thing.createThing(1,2,3,4,5,6,7);
