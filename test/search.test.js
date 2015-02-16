f8d3df6fa416d6108739aacc661a08b6ceba9207
vicki388 <vickieverts1@gmail.com>
2015-02-16 15:18:20 -0700

    modified test

--- test/search.test.js
+++ test/search.test.js
@@ -5,48 +5,7 @@ var streamify = require('stream-array'),
     fileExists = require('file-exists');
 
 describe('thing', function() {
-// describe('search', function() {
-     //this.timeout(35000);
 
-//     it('should change the text in a cell to a link from Google', function(done) {
-
-//         streamify([
-//             ['google', 'b', 'c']
-//         ])
-//             .pipe(refine.start())
-//             .pipe(select.cols(0, transform.search()))
-//             .pipe(refine.end())
-
-//             .pipe(assert.first(function(row) {
-//                 row.should.be.eql(['http://www.google.com/', 'b', 'c'])
-//             }))
-//             .pipe(assert.end(done))
-
-//     })
-
-
-// })
-
-// describe('searchByUser', function() {
-//     this.timeout(5000);
-
-//     it('should return true if the thingiverse work by a particular user has been saved to file', function(done) {
-
-//         streamify([
-//             ['mowi', 'zefram']
-//         ])
-//                 .pipe(thing.searchByUser())
-//                 .pipe(assert(function(data) {
-//                     //data = file-exists('file/Iphone 4 holder for Honda Jazz.zip')
-//                     file = console.log(fileExists('../examples/html_json.txt'))
-//                     file.should.be.eql(true)
-//                 }))
-//                 //.pipe(assert.length(1))
-//                 .pipe(assert.end(done))
-
-//         })
-//     })
-// })
 describe('searchByUser()', function() {
     this.timeout(10000);
         it('should save to file the first 6 works from the user', function(done) {
@@ -88,12 +47,12 @@ describe('searchByTag()', function() {
         })
     })
 
-// describe('getThingByID()', function() {
+// describe('getFileByID()', function() {
 //     this.timeout(10000);
 //         it('should save to file the first 6 works matching the term', function(done) {
 
             
-//             thing.getThingByID('996355', 'ex')
+//             thing.getFileByID('996355', 'ex')
 //             assert.first(function(row) {
 //                 row.should.be.eql('saved')
 //             })
@@ -115,30 +74,17 @@ describe('searchByTag()', function() {
 //         })
 //     })
 
-// describe('createThing()', function() {
-//     this.timeout(10000);
-//         it('should save to file the first 6 works matching the term', function(done) {
+describe('createThing()', function() {
+    this.timeout(10000);
+        it('should create a thing with name, license, category, description, instruction, is_wip, tags', function(done) {
 
             
-//             thing.createThing(1,2,3,4,5,6,7)
-//             foundFile = fileExists('../files/Shoulder for Robot InMoov.zip')
-//             foundFile.should.be.eql(true)
-//             done()
+            thing.createThing(1,2,3,4,5,6,7)
+            assert.first(function(row) {
+                row.should.be.eql('done')
+            })
+            done()
                 
-//         })
-//     })
-
-// describe('getThingsFromTo()', function() {
-//     this.timeout(10000);
-//         it('should save to file the first 6 works matching the term', function(done) {
-
-            
-//             thing.getThingsFromTo('robot')
-//             foundFile = fileExists('../files/Shoulder for Robot InMoov.zip')
-//             foundFile.should.be.eql(true)
-//             done()
-                
-//         })
-//     })
+        })
+    })
 })
- //console.log(fileExists('../examples/html_json.txt'))
