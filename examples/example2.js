494b884dba9a242a008e7167b489e7dfcc67d0da
qubick <qubick.kim@gmail.com>
2015-02-27 15:47:49 -0700

    function

--- examples/example2.js
+++ examples/example2.js
@@ -2,7 +2,7 @@ var thing = require('../lib')
 
 //thing.searchByWord('robot')//,
 //thing.searchByTag('figurine')//,
-//thing.getFileById('996355')//,
+thing.getFileById('72826')//,
 //thing.getFilesFromTo('274500-274541')//,
 //thing.createThing(1,2,3,4,5,6,7);
-thing.getThingInfo(541095)
+//thing.getThingInfo(541095)
