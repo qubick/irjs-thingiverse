f4e004844c447470398826cb069351e607c46b69
qubick <qubick.kim@gmail.com>
2018-10-16 21:28:01 -0600

    retrieve author name who submitted at least one thing. AUthor profile page is not shown yet

--- examples/example2.js
+++ examples/example2.js
@@ -2,7 +2,8 @@ var thing = require('../lib')
 
 //thing.searchByWord('robot')//,
 //thing.searchByTag('figurine')//,
-thing.getFileById('72826')//,
-//thing.getFilesFromTo('274500-274541')//,
+//thing.getFileById('72826')//,
+//thing.getFilesFromTo('100-3000')//,
 //thing.createThing(1,2,3,4,5,6,7);
 //thing.getThingInfo(541095)
+thing.getAuthorProfiles('3147500-3147921')
