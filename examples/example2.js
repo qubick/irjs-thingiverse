01fddb7a94a0143b4bf3b81b290241df25e64cb7
qubick <qubick.kim@gmail.com>
2019-11-12 16:49:31 -0600

    find and logs thing tags

--- examples/example2.js
+++ examples/example2.js
@@ -7,4 +7,7 @@ var thing = require('../lib')
 //thing.createThing(1,2,3,4,5,6,7);
 //thing.getThingInfo(541095)
 //thing.getAuthorProfiles('3147921-3147921')
-thing.filterThings('3167262-3167262')
+
+
+//thing.filterThings('3168400-3168500') //reduce the range, so it's not blocked by Thingiverse
+thing.filterThings('3168451-3168451')
